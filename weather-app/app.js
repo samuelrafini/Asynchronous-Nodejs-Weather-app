543df777fadc11cc5c66df7f714ad5fb1c663784
Samuelrafini <samuelrafini@gmail.com>
2017-12-17 17:57:14 +0100

    Abstracting Callbacks

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,8 @@
-const request = require('request');
-//1301 Lombard street
-
 const yargs = require('yargs');
-const argv = yargs.
-options({
+const geocode = require('./geocode/geocode');
+
+const argv = yargs
+.options({
     a: {
         demand: true,
         alias:'address',
@@ -15,23 +14,18 @@ options({
 .alias('help', 'h')
 .argv;
 
-var encodeAddress = encodeURIComponent(argv.a);
-console.log(argv);
-
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeAddress}&key=AIzaSyDhJxH0l8R7vsySB1YXcjtU19JArIXIPLw`,
-    json: true
-}, (error, respond, body) =>{
-    if(error){
-        console('Unable to connect to user servers');
-    }else if(body.status === 'ZERO_RESULTS'){
-        console.log('Unable to find adress');
-    }else if(body.status === 'OK'){
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longtitude: ${body.results[0].geometry.location.lng}`);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage){
+        console.log(errorMessage);
+    }else {
+        console.log(JSON.stringify(results, undefined, 2));
     }
 });
+
+
+
+
+
 
 
 
