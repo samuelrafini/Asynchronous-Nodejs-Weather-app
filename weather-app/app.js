95d427fbe469b05dd1f3cb2d6e8f8bf681a998e7
Samuelrafini <samuelrafini@gmail.com>
2017-12-17 13:50:02 +0100

    Encoding User Input

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,14 +1,32 @@
 const request = require('request');
+//1301 Lombard street
+
+const yargs = require('yargs');
+const argv = yargs.
+options({
+    a: {
+        demand: true,
+        alias:'address',
+        describe: 'address to fetch weather for',
+        string: true
+    }
+})
+.help()
+.alias('help', 'h')
+.argv;
+
+var encodeAddress = encodeURIComponent(argv.a);
+console.log(argv);
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301+lombard+st+philadelphia&key=AIzaSyDhJxH0l8R7vsySB1YXcjtU19JArIXIPLw',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeAddress}&key=AIzaSyDhJxH0l8R7vsySB1YXcjtU19JArIXIPLw`,
     json: true
 }, (error, respond, body) =>{
     console.log(`Address: ${body.results[0].formatted_address}`);
     console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
     console.log(`Longtitude: ${body.results[0].geometry.location.lng}`);
-    
-    
 });
+
+
 
 
