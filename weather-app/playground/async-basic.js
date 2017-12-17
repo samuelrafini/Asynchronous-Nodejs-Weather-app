389a13634c95ed22ff8fe57c5a76cbac4b38ad44
Samuelrafini <samuelrafini@gmail.com>
2017-12-17 10:25:18 +0100

    Async Basics

---
+++ weather-app/playground/async-basic.js
@@ -0,0 +1,12 @@
+//Async Basics
+console.log('Starting app');
+
+setTimeout(() => {
+    console.log('Inside of callback');
+}, 2000);
+
+setTimeout(() => {
+    console.log('Second setTimeout');
+}, 0);
+
+console.log('Finishing up');
