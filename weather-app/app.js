95fd1bdd2a237d5fc234fc9ae154f0ef01cf65f3
Samuelrafini <samuelrafini@gmail.com>
2017-12-17 13:19:06 +0100

    What's Makes up an HTTP Request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,14 @@
 const request = require('request');
 
 request({
-    url: 'http://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301+lombard+st+philadelphia&key=AIzaSyDhJxH0l8R7vsySB1YXcjtU19JArIXIPLw',
     json: true
 }, (error, respond, body) =>{
-    console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longtitude: ${body.results[0].geometry.location.lng}`);
+    
+    
 });
+
 
